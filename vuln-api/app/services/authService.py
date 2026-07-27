f75d6a69e6e3942024854094694ef9876dd8e3cb
AresOnee <vladimir.vidal@usach.cl>
2026-07-26 23:58:24 -0400

    fix swagger

--- vuln-api/app/services/authService.py
+++ vuln-api/app/services/authService.py
@@ -17,7 +17,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 def verify_password(plain, hashed):
